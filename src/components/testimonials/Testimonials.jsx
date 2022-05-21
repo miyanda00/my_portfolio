be45b44abd8ec660cd1c36f5a27ea62fa85cbe7b
Micheal Iyanda <myke1332@gmail.com>
2022-05-22 00:11:51 +0100

    6th commit-Testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+
+// import Swiper core and required modules
+import { Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name:'The Python Software Foundation',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quasi, asperiores enim doloremque quam fugit ab tempora possimus illum at expedita placeat assumenda aut maiores, voluptate nemo explicabo? Sed, corporis.',
+  },
+
+  {
+    avatar: AVTR2,
+    name:'The Python Software Foundation',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quasi, asperiores enim doloremque quam fugit ab tempora possimus illum at expedita placeat assumenda aut maiores, voluptate nemo explicabo? Sed, corporis.',
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h2>Mentions and Awards</h2>
+
+      <Swiper className="container testimonials__container"
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1} 
+      pagination={{ clickable: true }}>
+      {
+        data.map(({avatar, name, review}, index) => {
+          return(
+            <SwiperSlide key={index} className='testimonial'>
+            <div className="client__avatar">
+              <img src={avatar} />
+            </div>
+            <h5 className='client__name'>{name}</h5>
+              <small className='client__review'> {review}</small>
+          </SwiperSlide>
+          )
+        })
+      } 
+      </Swiper>
+    </section>
   )
 }
 
