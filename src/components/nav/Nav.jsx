63fbe9ba907d656f5c54ff2c80c38c18bdaaf8c5
Micheal Iyanda <myke1332@gmail.com>
2022-05-21 12:13:05 +0100

    third commit

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {ImHome} from 'react-icons/im'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsBookmarkStarFill} from 'react-icons/bs'
+import {GiFiles} from 'react-icons/gi'
+import {MdContactPage} from 'react-icons/md'
+import{useState} from 'react'
+
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() =>setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}><ImHome /></a>
+      <a href="#about" onClick={() =>setActiveNav('#about')} className={activeNav ==='#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() =>setActiveNav('#experience')} className={activeNav ==='#experience' ? 'active' : ''}><BsBookmarkStarFill /></a>
+      <a href="#porfolio" onClick={() =>setActiveNav('#porfolio')} className={activeNav ==='#porfolio' ? 'active' : ''}><GiFiles /></a>
+      <a href="#contact" onClick={() =>setActiveNav('#contact')} className={activeNav ==='#contact' ? 'active' : ''}><MdContactPage /></a>
+    </nav>
   )
 }
 
